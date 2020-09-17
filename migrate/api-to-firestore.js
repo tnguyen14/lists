b9a1d44c1486a95334b7d72bb02e949f37bb3db3
Tri Nguyen <tri@tridnguyen.com>
2020-09-17 00:46:34 -0400

    Migrate multiple lists

--- migrate/api-to-firestore.js
+++ migrate/api-to-firestore.js
@@ -12,41 +12,60 @@ const {
 
 const apiToken = process.env.API_TOKEN;
 const apiServer = process.env.MIGRATE_FROM_API_SERVER;
-const listType = process.env.LIST_TYPE;
-const listName = process.env.LIST_NAME;
+
+const listsToMigrate = [
+  {
+    type: "checkbook",
+    name: "business-checking",
+  },
+  {
+    type: "checkbook",
+    name: "personal-checking",
+  },
+  {
+    type: "checkbook",
+    name: "square-checking",
+  },
+  {
+    type: "read",
+    name: "tri",
+  },
+];
 
 const user = jwt.decode(apiToken);
 
-Promise.all([
-  getJson(`${apiServer}/${listType}/${listName}`, {
-    headers: {
-      Authorization: `Bearer ${apiToken}`,
-    },
-  }),
-  getJson(`${apiServer}/${listType}/${listName}/items`, {
-    headers: {
-      Authorization: `Bearer ${apiToken}`,
-    },
-  }),
-])
-  .then(async ([list, items]) => {
-    console.log(`Migrating list ${listType}!${listName}`);
-    console.log(list);
-    // list might not exist - wrap in try/catch
-    try {
-      console.log("Deleting old list...");
-      await deleteList(user, listType, listName);
-    } catch (e) {
-      console.error(e);
-    }
-    console.log("Creating new list...");
-    await createList(user, listType, listName);
-    console.log("updating new list with metadata...");
-    await updateList(user, listType, listName, list);
-    console.log(`adding ${items.length} items in bulk`);
-    await addItemsBulk(user, listType, listName, items);
-  })
-  .then(null, (err) => {
-    console.error(err);
-    process.exit(1);
-  });
+async function migrateList({ type: listType, name: listName }) {
+  const [list, items] = await Promise.all([
+    getJson(`${apiServer}/${listType}/${listName}`, {
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+      },
+    }),
+    getJson(`${apiServer}/${listType}/${listName}/items`, {
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+      },
+    }),
+  ]);
+  const listId = `${listType}!${listName}`;
+  console.log(`Migrating list ${listId}`);
+  // list might not exist - wrap in try/catch
+  try {
+    console.log(`Deleting old list ${listId}`);
+    await deleteList(user, listType, listName);
+  } catch (e) {
+    console.error(e);
+  }
+  console.log(`Creating new list ${listId}`);
+  await createList(user, listType, listName);
+  console.log(`Updating new list ${listId} with metadata`);
+  await updateList(user, listType, listName, list);
+  console.log(`Adding ${items.length} items to list ${listId}`);
+  await addItemsBulk(user, listType, listName, items);
+}
+
+try {
+  listsToMigrate.map(migrateList);
+} catch (e) {
+  console.error(e);
+}
