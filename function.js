7a0a3b6f1e99e46b8476f5302c92ab9ee0f994d8
Tri Nguyen <tri@tridnguyen.com>
2020-10-19 21:54:31 -0400

    Add migrate cloud function

---
+++ function.js
@@ -0,0 +1,13 @@
+const { migrate } = require("./migrate/api-to-firestore");
+
+exports.migrate = async (req, res) => {
+  try {
+    migrate();
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (e) {
+    res.status(500).send(e.message);
+    return;
+  }
+};
