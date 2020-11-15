9b7d04cb794b68a32533fa2c5a8d82de6a55cf5e
Tri Nguyen <tri@tridnguyen.com>
2020-11-15 17:28:02 -0500

    Migrate ledge transactions

--- migrate/ledge.js
+++ migrate/ledge.js
@@ -2,8 +2,54 @@ require("dotenv").config();
 const authToken = process.env.AUTH_TOKEN;
 const LISTS_SERVER_URL = process.env.API_SERVER;
 const { getJson, patchJson, postJson, deleteJson } = require("simple-fetch");
+const qs = require("qs");
 
 const OLD_SERVER_URL = "https://ledge.cloud.tridnguyen.com";
+
+async function migrateTransactionsInChunk(authToken, before) {
+  let query = {
+    limit: 500,
+  };
+  if (before) {
+    query.before = before;
+  }
+
+  const transactions = await getJson(
+    `${OLD_SERVER_URL}/accounts/daily/transactions?${qs.stringify(query)}`,
+    {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }
+  );
+  if (!transactions.length) {
+    console.log("No more transactions");
+    return;
+  }
+
+  console.log(
+    `Migrating ${transactions.length} transactions, starting with ${
+      transactions[0].date
+    }, ending with ${transactions[transactions.length - 1].date}`
+  );
+
+  const addResponse = await postJson(
+    `${LISTS_SERVER_URL}/ledge/tri/bulk`,
+    transactions,
+    {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }
+  );
+  if (!addResponse.success) {
+    throw new Error("Failed to add transactions");
+  }
+  await migrateTransactionsInChunk(
+    authToken,
+    transactions[transactions.length - 1].date
+  );
+}
 
 async function migrate() {
   const account = await getJson(`${OLD_SERVER_URL}/accounts/daily`, {
@@ -55,7 +101,7 @@ async function migrate() {
   if (!updateMeta.success) {
     throw new Error("Unable to update account meta");
   }
-  return updateMeta;
+  await migrateTransactionsInChunk(authToken);
 }
 
 migrate().then(console.log, console.error);
