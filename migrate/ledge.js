24a093fa07864c7daee3f566276d174404f2c0ee
Tri Nguyen <tri@tridnguyen.com>
2020-11-15 17:02:11 -0500

    Update migrate ledge to delete and re-create list

--- migrate/ledge.js
+++ migrate/ledge.js
@@ -1,11 +1,35 @@
 require("dotenv").config();
 const authToken = process.env.AUTH_TOKEN;
-const apiServer = process.env.API_SERVER;
-const { getJson, patchJson, postJson } = require("simple-fetch");
+const LISTS_SERVER_URL = process.env.API_SERVER;
+const { getJson, patchJson, postJson, deleteJson } = require("simple-fetch");
+
+const OLD_SERVER_URL = "https://ledge.cloud.tridnguyen.com";
 
 async function migrate() {
-  const account = await getJson(
-    "https://ledge.cloud.tridnguyen.com/accounts/daily",
+  const account = await getJson(`${OLD_SERVER_URL}/accounts/daily`, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
+
+  try {
+    await deleteJson(`${LISTS_SERVER_URL}/ledge/tri`, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+  } catch (e) {
+    if (e.response.statusText != "Not Found") {
+      throw e;
+    }
+  }
+
+  await postJson(
+    `${LISTS_SERVER_URL}`,
+    {
+      type: "ledge",
+      name: "tri",
+    },
     {
       headers: {
         Authorization: `Bearer ${authToken}`,
@@ -14,7 +38,7 @@ async function migrate() {
   );
 
   const updateMeta = await patchJson(
-    `${apiServer}/ledge/tri`,
+    `${LISTS_SERVER_URL}/ledge/tri`,
     {
       meta: {
         categories: account.categories,
