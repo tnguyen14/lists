3ff048d9450de36dbf984e96024a6da6642c9ee0
Tri Nguyen <tri@tridnguyen.com>
2023-11-18 13:20:08 -0500

    Add Svelte-based UI (#2)

---
+++ ui/svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
