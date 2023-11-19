87db36dbe41cde77034ac1e2d8c0d48b92cbc643
Tri Nguyen <tri@tridnguyen.com>
2023-11-19 19:13:27 +0000

    Load auth

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import createAuth from '@tridnguyen/auth/spa';
+	onMount(async () => {
+		const auth0 = await createAuth({
+			clientId: 'dXrVfRywvgZJcf5J1z74sGXmgDfsK5AK'
+		});
+		console.log(await auth0.isAuthenticated());
+	});
+</script>
+
+<div class="main">
+	<h1>Lists</h1>
+	<p>
+		<button>Log In</button>
+	</p>
+</div>
+
+<style>
+	.main {
+		margin: 2rem auto 1rem;
+		width: 90%;
+	}
+	p {
+		text-align: center;
+	}
+</style>
